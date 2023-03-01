30905774ef7213d39cdd4240b5269c1407716283
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-28 21:04:55 -0600

    create index and new function

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from "./ProductsNew";
+
 export function Content() {
+  const [products, setProducts] = useState([]);
+  const handleIndexProducts = () => {
+    console.log("handleIndexProducts");
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  const handleCreateProduct = (params, successCallback) => {
+    console.log("handleCreatePhoto", params);
+    axios.post("http://localhost:3000/products.json", params).then((response) => {
+      setProducts([...products, response.data]);
+      successCallback();
+    });
+  };
+
+  useEffect(handleIndexProducts, []);
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <ProductsIndex products={products} />
+      <ProductsNew onCreateProduct={handleCreateProduct} />
     </div>
   );
 }
